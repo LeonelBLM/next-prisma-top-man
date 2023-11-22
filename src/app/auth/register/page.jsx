2259949b77fb85ea4afa337ff17dda3bb38fa074
3016lye@gmail.com <jump_jet@live.com>
2023-11-22 18:56:27 -0400

    api de registro y pagina de registro terminada

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,14 +1,39 @@
 "use client"
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import "../../styles/login.css"
 
 function RegisterPage() {
 
-  const {register, handleSubmit} = useForm();
+  const {
+    register, 
+    handleSubmit, 
+    formState: {errors},
+  } = useForm();
 
-  const onSubmit = handleSubmit(data => {
-    console.log(data)
-  })
+  const router = useRouter()
+
+  const onSubmit = handleSubmit(async (data) => {
+    if (data.password !== data.confirmPassword){
+      return alert('Password do not match');
+    }
+
+
+    const res = await fetch('/api/auth/register', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    if (res.ok) {
+      router.push('/auth/login')
+    }
+  });
 
   return (
     <section id="hero" className="d-flex align-items-center justify-content-center py-4">
@@ -20,33 +45,68 @@ function RegisterPage() {
               <label className="form-label mt-4">Usuario</label>
               <input type="text" className="form-control" id="title" aria-describedby="textarea" placeholder="Usuario" 
                 { ...register("username", {
-                  required: true,
+                  required: {
+                    value: true,
+                    message: 'username is required'
+                  }
                 })}
               />
+
+            {
+              errors.username && (
+                <span>{errors.username.message}</span>
+              )
+            }
+
+
             </div>
             <div className="form-group">
               <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" 
               { ...register("email", {
-                required: true,
+                required: {
+                  value: true,
+                  message: 'email is required'
+                }
               })}
               />
+              {
+              errors.email && (
+                <span>{errors.email.message}</span>
+              )
+            }
             </div>
             <div className="form-group">
               <label htmlFor="exampleInputPassword1" className="form-label mt-4">Password</label>
-              <input type="password" className="form-control" id="" placeholder="Password" autocomplete="off"
+              <input type="password" className="form-control" id="password" placeholder="Password"
               { ...register("password", {
-                required: true,
+                required: {
+                  value: true,
+                  message: 'password is required',
+                }
               })}
               />
+              {
+              errors.password && (
+                <span>{errors.password.message}</span>
+              )
+            }
             </div>
             <div className="form-group">
               <label htmlFor="exampleInputPassword1" className="form-label mt-4">Confirm Password</label>
-              <input type="password" className="form-control" id="" placeholder="Confirm Password" autocomplete="off"
+              <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm Password"
               { ...register("confirmPassword", {
-                required: true,
+                required: {
+                  value: true,
+                  message: 'confim password is required'
+                }
               })}
               />
+              {
+              errors.confirmPassword && (
+                <span>{errors.confirmPassword.message}</span>
+              )
+            }
             </div>
           </fieldset>
           <div className="d-flex mt-4">
